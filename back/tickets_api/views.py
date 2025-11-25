595189db4f5444ea5a70d5f0da8dc8f15ca1b0f9
aeshmurodov <azizbeksamsung9@gmail.com>
2025-11-25 13:16:09 +0300

    Added some API fake leak

--- back/tickets_api/views.py
+++ back/tickets_api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions
 from .models import Ticket
 from .serializers import TicketSerializer
 
+API_KEY=123456
+
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     permission_classes = [permissions.IsAuthenticated]
