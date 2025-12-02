640928db2cb23cd0042bf4b2c23b1e9fbed17bd6
aeshmurodov <azizbeksamsung9@gmail.com>
2025-12-02 05:56:48 +0300

    Added pytest ini

--- back/tickets_api/serializers.py
+++ back/tickets_api/serializers.py
@@ -5,8 +5,3 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ['id','ticket_number','movie_title','start_time','seat_number']
-    
-    def validate_price(self, value):
-        if value < 500:
-            raise serializers.ValidationError("Минимальная сумма покупки — 500 рублей")
-        return value
