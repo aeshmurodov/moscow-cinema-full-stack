e686d20c5ef4dc98b937b5bbe11a6ca536e8dd95
леонид попов <leonid-popov-05@mail.ru>
2025-12-07 23:05:29 +0300

    sqlcheck_4

--- back/scripts/check_sql_security.sh
+++ back/scripts/check_sql_security.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-find db_schema -name "*.sql" | xargs sqlcheck -f
+set -e
+
+echo "[INFO] Creating virtual environment..."
+python3 -m venv venv
+
+echo "[INFO] Activating virtual environment..."
+source venv/bin/activate
+
+echo "[INFO] Installing sqlcheck..."
+pip install --upgrade pip
+pip install sqlcheck
+
+find back/db_schema -name "*.sql" | xargs sqlcheck -f
 if [ $? -ne 0 ]; then
     echo "Проверка безопасности SQL не удалась!"
     exit 1
